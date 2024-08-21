a987af6d4b8f288a998738d8f02948db722053f5
golden-hamster <isack19999@gmail.com>
2024-08-22 04:42:58 +0900

    Feat: 더미 데이터에 일정 추가

--- src/main/java/com/nbcam/schedule_management_v2/InitDb.java
+++ src/main/java/com/nbcam/schedule_management_v2/InitDb.java
@@ -1,5 +1,6 @@
 package com.nbcam.schedule_management_v2;
 
+import com.nbcam.schedule_management_v2.entity.Schedule;
 import com.nbcam.schedule_management_v2.entity.User;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
@@ -21,13 +22,25 @@ public class InitDb {
     public void init(){
         for (int i = 0; i < 3; i++) {
             User user = User.builder()
-                    .name("정이삭")
-                    .email("isjung7057@gmail.com")
+                    .name("정이삭" + i)
+                    .email("isjung" + i + "@gmail.com")
                     .createdAt(LocalDateTime.now())
                     .modifiedAt(LocalDateTime.now())
                     .password("비밀번호입니다.")
                     .build();
             em.persist(user);
+
+            for (int j = 0; j < 10; j++) {
+                Schedule schedule = Schedule.builder()
+                        .user(user)
+                        .title("일정제목입니다." + i + j)
+                        .content("일정내용입니다." + i + j)
+                        .createdAt(LocalDateTime.now())
+                        .modifiedAt(LocalDateTime.now())
+                        .build();
+
+                em.persist(schedule);
+            }
         }
     }
 }
